967c113ff19fc7c4286eacd0664fa20ae18345ec
Alicja Jurczak <ala.jurczak@gmail.com>
2023-03-21 20:37:21 +0100

    Normalize added to index.js and removed normalize from index.html

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import reportWebVitals from './reportWebVitals';
 import { GlobalStyle } from './GlobalStyle';
 import { theme } from "./theme";
 import { ThemeProvider } from 'styled-components';
+import { Normalize } from 'styled-normalize';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ThemeProvider theme={theme}>
     <React.StrictMode>
       <GlobalStyle />
+      <Normalize />
       <App />
     </React.StrictMode>
   </ThemeProvider>
