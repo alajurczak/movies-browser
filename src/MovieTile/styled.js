838a242292a5a165d9cf6f609c86e966aca40c3b
Paweł <pawelnackowski@gmail.com>
2023-03-22 18:50:39 +0100

    changed styled

--- src/MovieTile/styled.js
+++ src/MovieTile/styled.js
@@ -1,26 +1,36 @@
 import styled from "styled-components";
-import { ReactComponent as VideoIcon } from "./Images/VideoIcon.svg";
+import noImage from "./Images/VideoIcon.svg";
 
 export const Wrapper = styled.div`
-  background-color: ${({ theme }) => theme.colors.white};
-  display: grid;
-  width: 324px;
-  height: 650px;
-  box-shadow: 0px 4px 12px rgba(186, 199, 213, 0.5);
-  border-radius: 5px; 
-  margin: 24px;
+    width: 100%;
+    height: 100%;
+    background: #ffffff;
+    display: grid;
+    grid-template-rows: auto 1fr;
+    transition: 0.5s;
+    box-shadow: 0px 4px 12px rgba(186, 199, 213, 1);
+    border-radius: 5px;
+    padding: 16px;
+
 `;
 
-export const Poster = styled.div`
-  margin: 16px;
-  height: 434px;
-  background-color: ${({theme}) => theme.colors.silver};
-`
+export const ImageWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    border-radius: 5px;
+    flex-basis: auto;
+    background-image: url(${noImage});
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 80%;
+    background-color: ${({ theme }) => theme.colors.silver};
+    color: ${({ theme }) => theme.colors.white};
+`;
 
-export const StyledVideoIcon = styled(VideoIcon)`
-  justify-content: center;
-  align-self: center;
-  color: white;
-  width: 30px;
-  height: 30px;
-`
+export const Image = styled.img`
+    width: 100%;
+    height: auto;
+    border-radius: 5px;
+    aspect-ratio: 2/3;
+  `;
