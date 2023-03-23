3cb1576a8ae900d1bf93ead81246a0842be3b60b
Jakub Gluch <jakub.gluch92@gmail.com>
2023-03-23 21:01:23 +0100

    added components to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import Header from "./common/Header";
+import PopularMovies from "./features/PopularMovies";
+import { Main } from "./common/Main";
 
 function App() {
   return (
     <>
       <Header />
+      <Main>
+        <PopularMovies />
+      </Main>
     </>
   );
 }
