8465b3b1dad496406c7ac9216ff0a9af6fb6b2f5
Paweł <pawelnackowski@gmail.com>
2023-03-23 18:02:53 +0100

    added color smoke in theme

--- src/theme.js
+++ src/theme.js
@@ -3,6 +3,7 @@ export const theme = {
         whisper: "#F5F5FA",
         white: "#FFFFFF",
         black: "#000000",
+        smoke: "#18181B",
         stormGray: "#74788B",
         mystic: "#E4E6F0",
         silver: "#C4C4C4",
