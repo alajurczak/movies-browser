21bfc6d1514e889ea9a338b75ce7243f1202f43a
Paweł <pawelnackowski@gmail.com>
2023-03-23 18:03:55 +0100

    added content, extra content, tag, star rates and votes components

--- src/MovieTile/index.js
+++ src/MovieTile/index.js
@@ -1,11 +1,29 @@
-import { Image, ImageWrapper, Wrapper } from "./styled"
+import { ImageWrapper, MovieWrapper, DescriptionWrapper, Content, ExtraContent, Tag, WrapperTag, Star, RatingWrapper, Votes, Rates, ContentWrapper } from "./styled"
 
 export const MovieTile = () => {
   return (
-    <Wrapper>
+    <MovieWrapper>
       <ImageWrapper>
-          <Image/>
+        {/* Poster downloaded from api */}
       </ImageWrapper>
-    </Wrapper>
+      <DescriptionWrapper>
+        <ContentWrapper>
+          <Content>Film bardzo bardzo bardzo bardzo długi tytuł</Content>
+          <ExtraContent>Rok</ExtraContent>
+          <WrapperTag>
+            <Tag>{"Tag"}</Tag>
+            <Tag>{"Tag"}</Tag>
+            <Tag>{"Tag"}</Tag>
+            <Tag>{"Tag"}</Tag>
+            <Tag>{"Tag"}</Tag>
+          </WrapperTag>
+        </ContentWrapper>
+        <RatingWrapper>
+          <Star />
+          <Rates>{"9.9"}</Rates>
+          <Votes>{"180"}</Votes>
+        </RatingWrapper>
+      </DescriptionWrapper>
+    </MovieWrapper>
   )
 }
