409d40f48f6bf8311769cc98b55da408e50fee20
Paweł <pawelnackowski@gmail.com>
2023-03-24 10:00:55 +0100

    added spin

--- src/Common/Loader/styled.js
+++ src/Common/Loader/styled.js
@@ -8,4 +8,13 @@ export const Loader = styled.div`
   border-top: 12px solid ${({theme}) => theme.colors.black};
   border-radius: 50%;
   animation: spin 1s linear infinite;
+
+     @keyframes spin {
+    0% {
+     transform: rotate(0deg);
+    };
+    100% {
+     transform: rotate(360deg);
+    };
+  };
 `
