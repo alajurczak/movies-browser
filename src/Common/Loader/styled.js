06a45c8e96045495ef83b0c7ac6daf782786337f
Paweł <pawelnackowski@gmail.com>
2023-03-25 09:43:24 +0100

    added header and and resolution matching

--- src/Common/Loader/styled.js
+++ src/Common/Loader/styled.js
@@ -2,7 +2,27 @@ import styled from "styled-components";
 const mobileMinBp = ({ theme }) => theme.breakpoints.mobileMin;
 const largeBp = ({ theme }) => theme.breakpoints.large;
 
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export const Header = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  margin: 56px 0 120px 12px;
+  transition: 0.5s;
+    @media (max-width: ${mobileMinBp}px){
+      margin: 24px 0 24px 12px;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 130%;
+    }
+`
+
 export const Loader = styled.div`
+  align-self: center;
   width: 91px;
   height: 91px;
   margin: 40px;
