7c09a3bb5cb831468c1cb84c06ed402a0c5534c4
Paweł <pawelnackowski@gmail.com>
2023-03-25 08:58:58 +0100

    added largeBp and transition when changing the page view

--- src/Common/Loader/styled.js
+++ src/Common/Loader/styled.js
@@ -1,14 +1,16 @@
 import styled from "styled-components";
 const mobileMinBp = ({ theme }) => theme.breakpoints.mobileMin;
+const largeBp = ({ theme }) => theme.breakpoints.large;
 
 export const Loader = styled.div`
   width: 91px;
   height: 91px;
   margin: 40px;
-  border: 12px solid ${({theme}) => theme.colors.snuff};
-  border-top: 12px solid ${({theme}) => theme.colors.black};
+  border: 12px solid ${({ theme }) => theme.colors.snuff};
+  border-top: 12px solid ${({ theme }) => theme.colors.black};
   border-radius: 50%;
   animation: spin 1s linear infinite;
+  transition: 0.5s;
     @keyframes spin {
       0% {
         transform: rotate(0deg);
@@ -17,7 +19,13 @@ export const Loader = styled.div`
         transform: rotate(360deg);
       };
   };
-     @media (max-width: ${mobileMinBp}px){
+    @media (max-width: ${largeBp}px){
+      width: 65px;
+      height: 65px;
+      border: 8px solid ${({ theme }) => theme.colors.snuff};
+      border-top: 8px solid ${({ theme }) => theme.colors.black};
+  }
+    @media (max-width: ${mobileMinBp}px){
       width: 35px;
       height: 35px;
       border: 4px solid ${({ theme }) => theme.colors.snuff};
