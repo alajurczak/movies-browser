e866a476d535da7e7714dfaf253f9779261450cc
Alicja Jurczak <ala.jurczak@gmail.com>
2023-03-25 19:41:17 +0100

    popularMoviesSlice created

---
+++ src/features/movies/popularMoviesSlice.js
@@ -0,0 +1,32 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const popularMoviesSlice = createSlice({
+    name: 'popularMovies',
+    initialState: {
+        status: "loading",
+        movies: [],
+    },
+    reducers: {
+        fetchMoviesLoading: state => { state.status = "loading" },
+        fetchMoviesError: state => { state.status = "error" },
+        fetchMoviesSuccess: (state, { payload: movies }) => {
+            state.status = "success",
+            state.movies = movies.results;
+            state.page = movies.page;
+        }
+    }
+});
+
+export const {
+    fetchMoviesLoading,
+    fetchMoviesError,
+    fetchMoviesSuccess
+} = popularMoviesSlice.actions;
+
+const selectMoviesState = state => state.popularMovies;
+
+export const selectStatus = state => selectMoviesState(state).status;
+export const selectMovies = state => selectMoviesState(state).movies;
+export const selectPage = state => selectMoviesState(state).page;
+
+export default popularMoviesSlice.reducer;
