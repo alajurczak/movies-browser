c77984094e04166efe9614db4bbadd9796307480
Paweł <pawelnackowski@gmail.com>
2023-03-26 15:31:02 +0200

    added responsiveness

--- src/Common/PagesNoResult/styled.js
+++ src/Common/PagesNoResult/styled.js
@@ -1,5 +1,7 @@
 import styled from "styled-components";
 import { ReactComponent as NoSearch } from "./Images/NoSearchImages.svg";
+const mobileMinBp = ({ theme }) => theme.breakpoints.mobileMax;
+const largeBp = ({ theme }) => theme.breakpoints.large;
 
 export const Wrapper = styled.div`
   display: flex;
@@ -12,8 +14,26 @@ export const Title = styled.h1`
   font-weight: 600;
   font-size: 36px;
   line-height: 120%;
+  transition: 0.5s;
+    @media (max-width: ${largeBp}px){
+      font-weight: 500;
+      font-size: 26px;
+      line-height: 120%;
+    }
+    @media (max-width: ${mobileMinBp}px){
+      font-weight: 500;
+      font-size: 20px;
+      line-height: 130%;
+    }
 `
 
 export const NoSearchImage = styled(NoSearch)`
   align-self: center;
+  transition: 0.5s;
+  @media (max-width: ${largeBp}px){
+    scale:80%;
+  }
+  @media (max-width: ${mobileMinBp}px){
+    scale: 50%;
+  }
   `
