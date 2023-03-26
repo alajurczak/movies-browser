82b3f96595c341e04189d92e378641af68808bb7
Paweł <pawelnackowski@gmail.com>
2023-03-26 15:17:54 +0200

    added styled title

--- src/Common/PagesNoResult/styled.js
+++ src/Common/PagesNoResult/styled.js
@@ -4,3 +4,9 @@ export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
 `
+export const Title = styled.h1`
+  margin-bottom: 40px;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+`
