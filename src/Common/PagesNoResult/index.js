ec1cebbb1fb4fff7dffcdce4b3454f94f29140c5
Paweł <pawelnackowski@gmail.com>
2023-03-26 15:18:13 +0200

    added title to pagesNoResult

--- src/Common/PagesNoResult/index.js
+++ src/Common/PagesNoResult/index.js
@@ -1,9 +1,9 @@
-import { Wrapper } from "./styled"
+import { Wrapper, Title } from "./styled"
 
 export const PagesNoResult = () => {
   return (
     <Wrapper>
-
+      <Title>Sorry, there are no results for “Muan”</Title>
     </Wrapper>
   )
 }
