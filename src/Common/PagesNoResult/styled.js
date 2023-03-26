d7b93c2f0d9df0982b71fd571ae7ad971f5440d9
Paweł <pawelnackowski@gmail.com>
2023-03-26 15:27:02 +0200

    added max-widtch 1368px

--- src/Common/PagesNoResult/styled.js
+++ src/Common/PagesNoResult/styled.js
@@ -1,9 +1,11 @@
 import styled from "styled-components";
-import { ReactComponent as NoSearch } from "./Images/NoSearchImages.svg"
+import { ReactComponent as NoSearch } from "./Images/NoSearchImages.svg";
 
 export const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
+  max-width: 1368px;
+  margin: auto;
 `
 export const Title = styled.h1`
   margin-bottom: 40px;
