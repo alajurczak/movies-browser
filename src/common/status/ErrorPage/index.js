a7029ca6ed6b5231c56fe3ebd61bdeef1fb24592
Paweł <pawelnackowski@gmail.com>
2023-03-27 12:49:07 +0200

    added button back to home page

--- src/common/status/ErrorPage/index.js
+++ src/common/status/ErrorPage/index.js
@@ -1,16 +1,17 @@
-import { Content, DangerIcon, Header, Wrapper } from "./styled";
+import { Button, Content, DangerIcon, Header, Wrapper } from "./styled";
 
 export const ErrorPage = () => {
   return (
     <Wrapper>
       <DangerIcon />
-        <Header>
-          Ooops! Something went wrong... 
-        </Header>
-        <Content>
-          Please check your network connection<br/>
-          and try again
-        </Content>
+      <Header>
+        Ooops! Something went wrong...
+      </Header>
+      <Content>
+        Please check your network connection<br />
+        and try again
+      </Content>
+      <Button>Back to home page</Button>
     </Wrapper>
   )
 };
