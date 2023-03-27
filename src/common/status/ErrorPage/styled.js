9aabe53b8133d56c881866bc9541e73e47255da0
Paweł <pawelnackowski@gmail.com>
2023-03-27 12:23:04 +0200

    added styled on header, added centered wrapper and added size danger icon

--- src/common/status/ErrorPage/styled.js
+++ src/common/status/ErrorPage/styled.js
@@ -3,10 +3,20 @@ import { ReactComponent as Danger } from "../Images/Danger.svg"
 
 export const Wrapper = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: end;
+  align-items: center;
 `
 
 export const DangerIcon = styled(Danger)`
+  margin: 14px;
+  width: 100px;
+  height: 100px;
+`
 
+export const Header = styled.h1`
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 120%;
+  color: ${({theme}) => theme.colors.smoke};
 `
