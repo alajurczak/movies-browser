5bc2d8b3ef0ba2de063dc37c1ea4c3a5a5d83de4
Paweł <pawelnackowski@gmail.com>
2023-03-29 17:12:00 +0200

    added hashRouter

--- src/index.js
+++ src/index.js
@@ -8,18 +8,21 @@ import { ThemeProvider } from "styled-components";
 import { Normalize } from "styled-normalize";
 import { Provider } from "react-redux";
 import store from "./store";
+import { HashRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider store={store}>
-    <ThemeProvider theme={theme}>
-      <React.StrictMode>
-        <GlobalStyle />
-        <Normalize />
-        <App />
-      </React.StrictMode>
-    </ThemeProvider>
-  </Provider>
+<HashRouter>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <React.StrictMode>
+          <GlobalStyle />
+          <Normalize />
+          <App />
+        </React.StrictMode>
+      </ThemeProvider>
+    </Provider>
+</HashRouter>
 );
 
 reportWebVitals();
