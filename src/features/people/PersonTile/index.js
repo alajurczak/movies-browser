a95c8632180984ef8a7eee31512bdb419f0f5712
Alicja Jurczak <ala.jurczak@gmail.com>
2023-04-01 21:54:24 +0200

    no Picture added into PersonTile

--- src/features/people/PersonTile/index.js
+++ src/features/people/PersonTile/index.js
@@ -1,11 +1,11 @@
 import { ProfileImage, Wrapper, Name } from "./styled"
-import Picture from './images/Picture.png';
+import noPicture from './images/Picture.png';
 import { imagesBaseUrl } from "../../../ApiPaths";
 
 export const PersonTile = ({ id, profile_path, name }) => {
     return (
         <Wrapper key={id}>
-            <ProfileImage src={`${imagesBaseUrl}/w342${profile_path}`} alt=""></ProfileImage>
+            <ProfileImage src={profile_path ? `${imagesBaseUrl}/w342${profile_path}` : noPicture} alt=""></ProfileImage>
             {name && <Name>{name}</Name>}
         </Wrapper>
     );
