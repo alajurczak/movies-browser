a861c709cdd5a9b1f535b66e57d302980d85eed2
Alicja Jurczak <ala.jurczak@gmail.com>
2023-04-01 19:35:25 +0200

    PopularPeople added into rootSaga and store

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,10 +1,12 @@
 import { all } from "redux-saga/effects";
 import { watchFetchPopularMovies } from "./features/movies/popularMoviesSaga";
 import { watchFetchGenres } from "./features/movies/MovieTile/Genre/genreSaga";
+import { watchFetchPopularPeople } from "./features/people/popularPeopleSaga";
 
 export default function* rootSaga() {
     yield all([
         watchFetchPopularMovies(),
         watchFetchGenres(),
+        watchFetchPopularPeople(),
     ]);
 };
--- src/store.js
+++ src/store.js
@@ -3,6 +3,7 @@ import popularMoviesReducer from './features/movies/popularMoviesSlice';
 import genreReducer from './features/movies/MovieTile/Genre/genreSlice';
 import createSagaMiddleware from "@redux-saga/core";
 import rootSaga from './rootSaga';
+import popularPeopleReducer from "./features/people/popularPeopleSlice";
 
 const SagaMiddleware = createSagaMiddleware();
 
@@ -10,6 +11,7 @@ const store = configureStore({
     reducer: {
         popularMovies: popularMoviesReducer,
         genres: genreReducer,
+        popularPeople: popularPeopleReducer,
     },
     middleware: [SagaMiddleware],
 });
