a857ed85a78acfd425d16c0375ed544187f200fd
Alicja Jurczak <ala.jurczak@gmail.com>
2023-04-02 20:25:38 +0200

    Api added into PersonPage

--- src/features/people/PersonPage/styled.js
+++ src/features/people/PersonPage/styled.js
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+const largeBp = ({ theme }) => theme.breakpoints.large;
+const mobileMaxBp = ({ theme }) => theme.breakpoints.mobileMax;
+const mediumBp = ({ theme }) => theme.breakpoints.medium;
 
 export const Wrapper = styled.div`
     max-width: 1368px;
@@ -14,5 +17,20 @@ export const ContentContainer = styled.div`
     gap: 24px;
     justify-content: center;
     grid-template-columns: repeat(4, 1fr);
+
+    @media (max-width: ${largeBp}px) {
+        grid-template-columns: repeat(3, 1fr);
+        gap: 16px;
+    };
+
+    @media (max-width: ${mobileMaxBp}px) {
+        grid-template-columns: repeat(2, 1fr);
+        gap: 16px;
+    };
+
+    @media (max-width: ${mediumBp}px) {
+        grid-template-columns: 1fr;
+        gap: 16px;
+    };
 `;
 
