ec943b4b7cd8c9c431c974851af52bbc90bdef8f
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-04-02 20:23:18 +0200

    GridList component created

---
+++ src/common/GridList/index.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+const mobileMediumBp = ({ theme }) => theme.breakpoints.medium;
+const mobileMaxBp = ({ theme }) => theme.breakpoints.mobileMax;
+const mobileLargeBp = ({ theme }) => theme.breakpoints.large;
+
+export const GridList = styled.div`
+  padding: 0;
+  margin: 0;
+  display: grid;
+  gap: 24px;
+  justify-content: center;
+  grid-template-columns: repeat(4, 1fr);
+  list-style: none;
+
+  @media (max-width: ${mobileLargeBp}px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: ${mobileMaxBp}px) {
+    gap: 16px;
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media (max-width: ${mobileMediumBp}px) {
+    gap: 16px;
+    grid-template-columns: 1fr;
+  } ;
+`;
