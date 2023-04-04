a97b467d9334770dc0e51f0e60d68d5dc52598f6
Jakub Głuch <106307102+AllmostHumann@users.noreply.github.com>
2023-04-04 22:03:16 +0200

    added main component

--- src/features/movies/MovieList/index.js
+++ src/features/movies/MovieList/index.js
@@ -2,7 +2,7 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { MovieList, StyledLink } from "./styled";
 import { Container } from "../../../common/Container";
-import { SectionTitle } from "../../../common/SectionTitle/styled";
+import { SectionTitle } from "../../../common/SectionTitle";
 import { MovieTile } from "../MovieTile";
 import { Pagination } from "../../../common/Pagination";
 import { Loading } from "../../../common/status/Loading";
@@ -13,6 +13,7 @@ import {
   selectStatus,
 } from "../../../features/movies/MovieTile/popularMoviesSlice";
 import { fetchGenres } from "../MovieTile/Genre/genreSlice";
+import { Main } from "../../../common/Main";
 
 const PopularMovies = () => {
   const dispatch = useDispatch();
@@ -31,39 +32,41 @@ const PopularMovies = () => {
       ) : stateOfLoading === "error" ? (
         <Error />
       ) : (
-        <Container>
-          <section>
-            <SectionTitle>Popular Movies</SectionTitle>
-            {movies && movies.length > 0 && (
-              <MovieList>
-                {movies.map(
-                  ({
-                    id,
-                    title,
-                    poster_path,
-                    vote_count,
-                    vote_average,
-                    release_date,
-                    genre_ids,
-                  }) => (
-                    <StyledLink to={`/movie/${id}`}>
-                      <MovieTile
-                        key={id}
-                        id={id}
-                        title={title}
-                        poster_path={poster_path}
-                        vote_average={vote_average}
-                        vote_count={vote_count}
-                        release_date={release_date}
-                        genre_ids={genre_ids}
-                      />
-                    </StyledLink>
-                  )
-                )}
-              </MovieList>
-            )}
-          </section>
-        </Container>
+        <Main>
+          <Container>
+            <section>
+              <SectionTitle>Popular Movies</SectionTitle>
+              {movies && movies.length > 0 && (
+                <MovieList>
+                  {movies.map(
+                    ({
+                      id,
+                      title,
+                      poster_path,
+                      vote_count,
+                      vote_average,
+                      release_date,
+                      genre_ids,
+                    }) => (
+                      <StyledLink to={`/movie/${id}`}>
+                        <MovieTile
+                          key={id}
+                          id={id}
+                          title={title}
+                          poster_path={poster_path}
+                          vote_average={vote_average}
+                          vote_count={vote_count}
+                          release_date={release_date}
+                          genre_ids={genre_ids}
+                        />
+                      </StyledLink>
+                    )
+                  )}
+                </MovieList>
+              )}
+            </section>
+          </Container>
+        </Main>
       )}
       <Pagination />
     </>
