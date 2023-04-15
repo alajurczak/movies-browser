5dbc246bf510be46c159152a831bd6246d5733f9
Alicja Jurczak <ala.jurczak@gmail.com>
2023-04-15 15:12:44 +0200

    added loader into movies page

--- src/features/movies/MovieTile/popularMoviesSaga.js
+++ src/features/movies/MovieTile/popularMoviesSaga.js
@@ -1,4 +1,4 @@
-import { call, put, takeLatest } from "redux-saga/effects";
+import { call, delay, put, takeLatest } from "redux-saga/effects";
 import {
   fetchMoviesSuccess,
   fetchMoviesError,
@@ -9,6 +9,7 @@ import { baseUrl, apiKey, language } from "../../../ApiPaths";
 
 function* fetchPopularMoviesHandler({ payload: page }) {
   try {
+    yield delay(1000);
     const popularMovies = yield call(
       getApi,
       `${baseUrl}/movie/popular${apiKey}${language}&page=${page}`
