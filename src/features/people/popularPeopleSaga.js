5cd5495ea33d3a74f5c12ace46e0e979ac23a032
Paweł <pawelnackowski@gmail.com>
2023-04-16 10:31:32 +0200

    added  path search person

--- src/features/people/popularPeopleSaga.js
+++ src/features/people/popularPeopleSaga.js
@@ -7,13 +7,15 @@ import {
 import { getApi } from "../../getApi";
 import { apiKey, baseUrl, language } from "../../ApiPaths";
 
-function* fetchPopularPeopleHandler({ payload: { page } }) {
+function* fetchPopularPeopleHandler({ payload: { page, query } }) {
+  const path =
+    query === null
+      ? `${baseUrl}/person/popular${apiKey}${language}&page=${page}`
+      : `${baseUrl}/search/person${apiKey}${language}&query=${query}`;
   try {
     yield delay(1000);
     const people = yield call(
-      getApi,
-      `${baseUrl}/person/popular${apiKey}${language}&page=${page}`
-    );
+      getApi, path );
     yield put(fetchPeopleSuccess(people));
   } catch (error) {
     yield put(fetchPeopleError());
